a20e49ca838efa670b238de5bb65dac8894c0600
Dasha Baklanova <dashabaklanova@Dashas-MacBook-Pro.local>
2021-02-21 14:37:10 -0500

    Added humidity result to forecast.js

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -8,7 +8,7 @@ const forecast = (latitude,longtitude, callback) => {
         } else if(response.body.error){
             callback('Unable to find location. Try another search.',undefined)
         } else {
-            callback(undefined,response.body.current.weather_descriptions[0]+ '. It is currently '+response.body.current.temperature+' degrees out. It feels like '+response.body.current.feelslike+' degrees out.')
+            callback(undefined,response.body.current.weather_descriptions[0]+ '. It is currently '+response.body.current.temperature+' degrees out. It feels like '+response.body.current.feelslike+' degrees out and humidity is ' + response.body.current.humidity)
         }
     })
 }
